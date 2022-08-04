c6cca183285dd47fb6ca6535fd53acf9f1a18e83
raqpoletto <polettoraq@gmail.com>
2022-08-04 17:58:48 +0100

    missing some btns

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,23 +1,51 @@
-const charactersAPI = new APIHandler('http://localhost:8000');
+const charactersAPI = new APIHandler("http://localhost:8000");
 
-window.addEventListener('load', () => {
-  document.getElementById('fetch-all').addEventListener('click', function (event) {
-
-  });
-
-  document.getElementById('fetch-one').addEventListener('click', function (event) {
-
-  });
-
-  document.getElementById('delete-one').addEventListener('click', function (event) {
-
-  });
-
-  document.getElementById('edit-character-form').addEventListener('submit', function (event) {
-
-  });
-
-  document.getElementById('new-character-form').addEventListener('submit', function (event) {
-
-  });
+window.addEventListener("load", () => {
+  document
+    .getElementById("fetch-all")
+    .addEventListener("click", function (event) {
+      charactersAPI.getFullList().then((response) => {
+        console.log(response);
+        const container = document.querySelector(".characters-container"); // estudar querySelector
+        response.data.forEach((character) => {
+          const div = document.createElement("div"); // estamos criando um div 'de raiz' // temos que buscar no API as info para criar um novo card, por isso criamos um div que não existe no html
+          div.innerHTML = `<div class="character-info">
+          <div class="name">${character.name}</div>
+          <div class="occupation">${character.occupation}</div>
+          <div class="cartoon">${character.cartoon}</div>
+          <div class="weapon">${character.weapon}</div>
+          </div>`;
+          container.appendChild(div);
+        });
+      });
+    });
 });
+
+document
+  .getElementById("fetch-one")
+  .addEventListener("click", function (event) {
+    charactersAPI.getOneRegister().then((response) => {
+      console.log(response);
+      const container = document.querySelector("characters-container");
+      const div = document.setAttribute("input");
+      input.innerHTML = `<div class="character-info">
+          <div class="name">${character.name}</div>
+          <div class="occupation">${character.occupation}</div>
+          <div class="cartoon">${character.cartoon}</div>
+          <div class="weapon">${character.weapon}</div>
+          </div>`;
+      container.appendChild(div);
+    });
+  });
+
+document
+  .getElementById("delete-one")
+  .addEventListener("click", function (event) {});
+
+document
+  .getElementById("edit-character-form")
+  .addEventListener("submit", function (event) {});
+
+document
+  .getElementById("new-character-form")
+  .addEventListener("submit", function (event) {});
